6eb263c5b3fb125150092adb4b18ba49e7870bc9
Mohamad Rizki Dafianto <mohamad.rizki@fusi24.com>
2018-10-24 12:06:16 +0700

    Add DataType

---
+++ app/src/main/java/com/dafian/android/mhwmobile/config/DataType.java
@@ -0,0 +1,50 @@
+package com.dafian.android.mhwmobile.config;
+
+public class DataType {
+
+    public class ElementType {
+
+        public static final String FIRE = "fire";
+        public static final String WATER = "water";
+        public static final String ICE = "ice";
+        public static final String THUNDER = "thunder";
+        public static final String DRAGON = "dragon";
+        public static final String BLAST = "blast";
+        public static final String POISON = "poison";
+        public static final String SLEEP = "sleep";
+        public static final String PARALYSIS = "paralysis";
+    }
+
+    public class WeaponType {
+
+        public static final String GREAT_SWORD = "great-sword";
+        public static final String LONG_SWORD = "long-sword";
+        public static final String SHIELD_SWORD = "sword-and-shield";
+        public static final String DUAL_BLADE = "dual-blades";
+        public static final String HAMMER = "hammer";
+        public static final String HUNTING_HORN = "hunting-horn";
+        public static final String LANCE = "lance";
+        public static final String GUN_LANCE = "gunlance";
+        public static final String SWITCH_AXE = "switch-axe";
+        public static final String CHARGE_BLADE = "charge-blade";
+        public static final String INSECT_GLAIVE = "insect-glaive";
+        public static final String LIGHT_BOWGUN = "light-bowgun";
+        public static final String HEAVY_BOWGUN = "heavy-bowgun";
+        public static final String BOW = "bow";
+    }
+
+    public class  ArmorType {
+
+        public static final String HEAD = "head";
+        public static final String CHEST = "chest";
+        public static final String GLOVES = "gloves";
+        public static final String WAIST = "waist";
+        public static final String LEGS = "legs";
+    }
+
+    public class ArmorRank {
+
+        public static final String LOW = "Low";
+        public static final String HIGH = "high";
+    }
+}
